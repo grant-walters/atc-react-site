3c02442a36ceeec644bead839a7edb93049b4744
Grant Walters <gwalters6968@sdsu.edu>
2023-02-02 13:10:35 -0800

    Set up default React-Create-App and made initial tweaks

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * App.js
+ * Main component for our app. The App component will the start of our UI tree
+ * (excluding React StrictMode wrapper)
+ */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		Test
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * index.js
+ * This file serves as our entry point when react bundles all our code.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+// Wraps app in Strict Mode that provides extra debugging info and warnings for children.
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
